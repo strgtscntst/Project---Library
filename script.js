f84d0e116e10bd3526676928836979496097875d
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2025-01-27 14:08:00 -0500

    feat add-book button, script, and CSS
    Add CSS for the table and new-book feature.
    Script adds form fields to new book, validates inputs, and adds it to
    the list if valid.
    Refactored table code to add a thead/tbody section for easier table
    clearing for repopulation.
    Renamed populateList() to updateTable().
    Moved dummy books to myLibrary{} intialization.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const myLibrary = [];
+// myLibrary initiation, with dummy content
+const myLibrary = [
+    {
+        title: "Holes",
+        author: "Louis Sachar",
+        pages: 233,
+    },
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: 328,
+    },
+    {
+        title: "The Eye of the World",
+        author: "Robert Jordan",
+        pages: 782,
+    }
+];
 
+// Book constructor
 function Book(title, author, pages){
     this.title = title;
     this.author = author;
@@ -10,9 +28,10 @@ function addBookToLibrary(book){
     myLibrary.push(book)
 }
 
-function populateList(library){
+// Add book list to the table
+function updateTable(library){
     let table = document.getElementById("bookList")
-    console.log(table)
+    table.innerHTML = "";
     for (let i = 0; i < library.length; i++){
         let row = document.createElement("tr");
         let book = library[i];
@@ -26,14 +45,46 @@ function populateList(library){
     }
 };
 
+const button = document.getElementById("addBookButton");
+
+button.addEventListener("click", () => {
+
+
+    // gather data from the fields
+    let name = document.getElementById("nameField").value;
+    let author = document.getElementById("authorField").value;
+    let pages = document.getElementById("pagesField").value;
+
+    
+    // check for empty fields and prevent adding if any are missing
+    if (!name || !author || !pages){
+        
+        // get angry
+        let angry = document.getElementById("itemsRequired")
+        angry.style.fontWeight = "bold";
+        angry.style.color = "red";
+        angry.style.fontSize = "300%";
+        angry.style.textAlign = "center"
+        return
+    }
+    
+    // ensure pages is a number
+    if (!name || !author || isNaN(pages)){
+        alert("Please fill in all fields correctly")
+        return
+    }
+
+    // create a new book object and add it to myLibrary[]
+    let book = new Book(name, author, pages);
+    myLibrary.push(book)
+
+    updateTable(myLibrary)
+})
 
 
 
-// Dummy Content
 
-const book1 = new Book("Holes", "Louis Sachar", 233);
-const book2 = new Book("The Hobbit", "J. R. R. Tolkien", 328);
-addBookToLibrary(book1);
-addBookToLibrary(book2);
 
-populateList(myLibrary);
+
+// Start scripts
+updateTable(myLibrary)
